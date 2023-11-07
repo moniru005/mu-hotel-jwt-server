aa0ca38d110cdbe5d18518f4f49c816a6eee3e28
Monir <moniru005@gmail.com>
2023-11-07 23:00:26 +0600

    add cors origin and credentials

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true
+}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2hcl8v.mongodb.net/?retryWrites=true&w=majority`;
 
